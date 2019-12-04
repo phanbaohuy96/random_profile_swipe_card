7055f85bb7593f8f460919eeb868a4c4a4eda4c8
Huy Bảo <baohuy.phan1996@gmail.com>
2019-12-04 21:16:33 +0700

    + add profile card page

---
+++ lib/ui/screen/profile_card_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilCardPage extends StatefulWidget {
+  @override
+  _ProfilCardPageState createState() => _ProfilCardPageState();
+}
+
+class _ProfilCardPageState extends State<ProfilCardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
